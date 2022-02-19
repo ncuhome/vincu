5678784be971d25d369fc73cf236dd7c2987df00
Kuss <2360314753@qq.com>
2022-02-20 01:51:23 +0800

    :rotating_light: fix: eslint config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ["module-resolver", {
-      "root": ["./src"],
-      "alias": {
-        "@": "./src",
-      }
-    }]
-  ]
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
