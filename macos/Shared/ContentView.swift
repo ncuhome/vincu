769a804f802668f05214e54121acba98ad5e70a3
Kuss <kussssss@outlook.com>
2022-05-26 23:40:57 +0800

    feat: macos initial

---
+++ macos/Shared/ContentView.swift
@@ -0,0 +1,148 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by kuss on 2022/5/26.
+//
+
+import SwiftUI
+import WebKit
+
+extension URL {
+    func query(_ queryParameterName: String) -> String? {
+        guard let url = URLComponents(string: self.absoluteString) else { return nil }
+        return url.queryItems?.first(where: { $0.name == queryParameterName })?.value
+    }
+}
+
+extension String {
+    var isValidURL: Bool {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
+            // it is a link, if the match covers the whole string
+            return match.range.length == self.utf16.count
+        } else {
+            return false
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var action = WebViewAction.idle
+    @State private var state = WebViewState.empty
+    @State private var address = "https://incu.ncuos.com"
+    private var contentController: WKUserContentController
+    
+    init() {
+        let script = """
+    window.appReady = true;
+    """
+
+        let userScript = WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        
+        let contentController = WKUserContentController()
+            
+        contentController.addUserScript(userScript)
+        
+        self.contentController = contentController
+    }
+    
+    
+    var body: some View {
+        navigationToolbar
+        Divider()
+        WebView(action: $action, state: $state, contentController: self.contentController).onAppear(perform: onLoad)
+            .handlesExternalEvents(preferring: Set(arrayLiteral: "*"), allowing: Set(arrayLiteral: "*"))
+            .onOpenURL(perform: {scheme in
+                print(scheme)
+                let url = scheme.query("url")
+                
+ 
+                if let nextUrl = url {
+                    if nextUrl.isValidURL {
+                        address = nextUrl;
+                        onLoad()
+                    }
+                }
+            })
+    }
+    
+    func onLoad() {
+        if let url = URL(string: address) {
+            action = .load(URLRequest(url: url))
+        }
+    }
+    
+    private var navigationToolbar: some View {
+        HStack(spacing: 10) {
+            TextField("Address", text: $address)
+                .onSubmit {
+                    onLoad()
+                }
+            if state.isLoading {
+                if #available(iOS 14, macOS 11, *) {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                } else {
+                    Text("Loading")
+                }
+            }
+            Spacer()
+            Button("Go") {
+                onLoad()
+            }
+            Button(action: {
+                action = .reload
+            }) {
+                if #available(iOS 14, macOS 11, *) {
+                    Image(systemName: "arrow.counterclockwise")
+                        .imageScale(.large)
+                } else {
+                    Text("Reload")
+                }
+            }
+            if state.canGoBack {
+                Button(action: {
+                    action = .goBack
+                }) {
+                    if #available(iOS 14, macOS 11, *) {
+                        Image(systemName: "chevron.left")
+                            .imageScale(.large)
+                    } else {
+                        Text("<")
+                    }
+                }
+            }
+            if state.canGoForward {
+                Button(action: {
+                    action = .goForward
+                }) {
+                    if #available(iOS 14, macOS 11, *) {
+                        Image(systemName: "chevron.right")
+                            .imageScale(.large)
+                    } else {
+                        Text(">")
+                    }
+                }
+            }
+            Button("Test") {
+                action = .evaluateJS("""
+                                     (function() {
+                                        alert(1);
+                                     })();
+""", noop);
+            }
+        }.padding(.horizontal, 10)
+            .padding(.top, 10)
+    }
+    
+    func noop(arg: Result<Any?, Error>) -> Void {
+        print(arg)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macos/Shared/vincuApp.swift
@@ -0,0 +1,18 @@
+//
+//  vincuApp.swift
+//  Shared
+//
+//  Created by kuss on 2022/5/26.
+//
+
+import SwiftUI
+
+@main
+struct vincuApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
